26d68abad75e1d4849c24f4e29abf5a2f85e190f
Abhijith12-projects <abhijithajay1998@gmail.com>
2023-10-08 20:54:52 +0530

    new

--- src/screens/users/Welcome.js
+++ src/screens/users/Welcome.js
@@ -1,39 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React from "react";
 
-function WelcomeScreen() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    axios
-      .get(
-        "https://authentication-d9563-default-rtdb.firebaseio.com/message/json"
-      )
-      .then((response) => {
-        setMessage(response.data);
-      });
-  }, []);
-
+const Welcome = () => {
   return (
-    <View style={styles.rootContainer}>
-      <Text style={styles.title}>Welcome!</Text>
-      <Text>You authenticated successfully!</Text>
+    <View>
+      <Text>Welcome</Text>
     </View>
   );
-}
+};
 
-export default WelcomeScreen;
+export default Welcome;
 
-const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 32,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 8,
-  },
-});
+const styles = StyleSheet.create({});
